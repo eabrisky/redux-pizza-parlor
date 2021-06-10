072e60f1c2a9714053a09f210b081f61b2cd0143
Emry Brisky <eabrisky@gmail.com>
2021-06-10 11:56:55 -0500

    set up store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pizzaReducer = (state = [], action) => {
+
+    return state;
+} // end pizzaReducer
+
+const customerReducer = (state = [], action) => {
+    
+    return state;
+} // end customerReducer
+
+const store = createStore(
+
+    combineReducers({
+        pizzaReducer, customerReducer
+    }),
+    applyMiddleware(
+        logger
+    )
+
+); // end createStore
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+document.getElementById('root')
+);
