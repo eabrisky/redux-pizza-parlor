6f06f297327f4a8486d9d435cc422015e334fcd6
Terry O'Keefe <terryokeefe@Mac-Mini.local>
2021-06-10 16:17:43 -0500

    fixing nav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,20 +8,24 @@ import Checkout from '../Checkout/Checkout';
 function App() {
 
   return (
-    <div>
-      <Welcome />
-  //   <div className='App'>
-  //     <header className='App-header'>
-  //       <h1 className='App-title'>Prime Pizza</h1>
-  //     </header>
-      
-  //     <img src='images/pizza_photo.png' />
-  //     <p>Pizza is great.</p>
-  
-  //   </div>
-      <CustomerForm />
-     <Checkout />
-    </div>
+    <Welcome />
+    // <div>
+    //   <div>
+    //     <Welcome />
+    //   </div>
+    //   <div className='App'>
+    //     <header className='App-header'>
+    //       <h1 className='App-title'>Prime Pizza</h1>
+    //   </header>
+    //     <img src='images/pizza_photo.png' />
+    //   <p>Pizza is great.</p>
+    //   </div>
+    //   <div>
+    //     <CustomerForm />
+    //   <Checkout />
+    //   </div>
+    // </div>
+    
   );
 }
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,58 +1,61 @@
 import React from 'react';
 import './Welcome.css';
 import { Route, HashRouter as Router, Link, useHistory } from 'react-router-dom';
+import SelectPizza from '../Select Pizza/SelectPizza';
+import CustomerForm from '../Customer Form/CustomerForm';
+import Checkout from '../Checkout/Checkout';
 
 // function for Home/Welcome component
 function Welcome() {
     return (
-        <div>
-            <div className='App'>
-                <div>
-                    <header className='App-header'>
-                        <h1 className='App-title'>Prime Pizza</h1>
-                    </header>
-                    <nav class="nav-bar">
-                        <ul>
-                            <nav>
-                                <li>
-                                    <Link to="/">Home</Link>
-                                </li>
-                                <li>
-                                    <Link to="/">Cart</Link>
-                                </li>
-                                <li>
-                                    <Link to="/">Customer Information</Link>
-                                </li>
-                                <li>
-                                    <Link to="/">Checkout</Link>
-                                </li>
-                            </nav>
-                            <Route path="/">
-                                <Welcome />
-                            </Route>
-                            <Route path="/cart">
-                                
-                            </Route>
-                            <Route path="/">
-
-                            </Route>
-                            <Route path="/">
-
-                            </Route>
-                        </ul>
-                    </nav>
-                </div>
-                <div>
-                    <img src='images/pizza_photo.png' />
-                </div>
-                <div>
-                    <button>Start Order</button>
-                </div>
-                <div>
-                    <p>Pizza is great.</p>
+        <Router>
+            <div>
+                <div className='App'>
+                    <div>
+                        <header className='App-header'>
+                            <h1 className='App-title'>Prime Pizza</h1>
+                        </header>
+                        <nav class="nav-bar">
+                            <ul>
+                                    <li>
+                                        <Link to="/">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/selectPizza">Cart</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/customerForm">Customer Information</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/checkout">Checkout</Link>
+                                    </li>
+                                {/* <Route path="/" exact>
+                                    <Welcome />
+                                </Route> */}
+                                <Route path="/selectPizza">
+                                    <SelectPizza />
+                                </Route>
+                                <Route path="/customerForm">
+                                    <CustomerForm />
+                                </Route>
+                                <Route path="/checkout">
+                                    <Checkout />
+                                </Route>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div>
+                        <img src='images/pizza_photo.png' />
+                    </div>
+                    <div>
+                        <button>Start Order</button>
+                    </div>
+                    <div>
+                        <p>Pizza is great.</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Router>
     )
 }
 
