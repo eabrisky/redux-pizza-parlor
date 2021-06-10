7fba60e6b6240b67d0a4b69c0fd1beda754f4d05
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-10 14:14:32 -0500

    saving before pull request

--- src/components/Customer Form/CustomerForm.jsx
+++ src/components/Customer Form/CustomerForm.jsx
@@ -1,51 +1,57 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-function CustomerForm () {
-    const [customerName, setCustomerName] = useState();
-    const [streetAddress, setStreetAddress] = useState();
-    const [city, setCity] = useState();
-    const [zip, setZip] = useState();
-    const [type, setType] = useState();
+function CustomerForm() {
+  const [customerName, setCustomerName] = useState();
+  const [streetAddress, setStreetAddress] = useState();
+  const [city, setCity] = useState();
+  const [zip, setZip] = useState();
+  const [type, setType] = useState();
 
-
-
-    return (
-        <form> 
-            <input 
-        placeholder="Name" 
-        value={customerName} 
-        onChange={(event) => setCustomerName(event.target.value)} 
+  return (
+    <form>
+      <input
+        placeholder="Name"
+        value={customerName}
+        onChange={(event) => setCustomerName(event.target.value)}
       />
-            <input
+      <input
         placeholder="Street Address"
         value={streetAddress}
         onChange={(event) => setStreetAddress(event.target.value)}
       />
-            <input
+      <input
         placeholder="City"
         value={city}
         onChange={(event) => setCity(event.target.value)}
       />
-            <input
+      <input
         placeholder="Zip"
         value={zip}
         onChange={(event) => setZip(event.target.value)}
       />
-        <div>
-            <input type="radio" id="pickup" value="Pickup" onChange={(event) => setType(event.target.value)} />
-            <label for="pickup">Pickup</label>
-        </div>
-        <div>
-            <input type="radio" id="delivery" value="Delivery" onChange={(event) => setType(event.target.value)} />
-            <label for="delivery">Delivery</label>
-        </div>
-        <div>
-            <input type="submit" value="Next" />
-        </div>
-
-
-        </form>
-    )
+      <div>
+        <input
+          type="radio"
+          id="pickup"
+          value="Pickup"
+          onChange={(event) => setType(event.target.value)}
+        />
+        <label for="pickup">Pickup</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="delivery"
+          value="Delivery"
+          onChange={(event) => setType(event.target.value)}
+        />
+        <label for="delivery">Delivery</label>
+      </div>
+      <div>
+        <input type="submit" value="Next" />
+      </div>
+    </form>
+  );
 }
 
 export default CustomerForm;
