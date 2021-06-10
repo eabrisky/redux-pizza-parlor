2ec2b3c590bed1853c621e41eb8fb1f2f3afcc52
Emry Brisky <eabrisky@gmail.com>
2021-06-10 13:17:22 -0500

    added components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
